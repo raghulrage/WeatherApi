a8a078bf62c67eb53bac3980ef34205be05bae42
raghulrage <46341793+raghulrage@users.noreply.github.com>
2020-06-04 15:29:43 +0530

    First Commit

---
+++ scripts/forcast.js
@@ -0,0 +1,29 @@
+const key = 'QRNCJAONK5qg9wZbkqRXTw4JnLeryzxU';
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const qurey = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + qurey);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const qurey = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + qurey);
+    const data = await response.json();
+    
+    return data[0]; 
+};
+
+// getCity('delhi').then(data => {
+//     return getWeather(data.Key);
+//  }).then( data => {
+//      console.log(data);
+//  }).catch(err => console.log(err));
